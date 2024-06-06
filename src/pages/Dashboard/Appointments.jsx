f0f7bbcf6d0dd6d17c3e8d7bcdd7203ca7e8b57e
Tajbir islam <studenttajbirislam@gmail.com>
2024-06-06 09:57:22 +0600

    stripe done and my upcomming appointment running

--- src/pages/Dashboard/Appointments.jsx
+++ src/pages/Dashboard/Appointments.jsx
@@ -3,6 +3,7 @@ import useMyAppointments from "../../hooks/useMyAppointments";
 import Title from "./Title";
 import Loading from "../../components/Loading";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const Appointments = () => {
   const [pagination, setPagination] = useState({
@@ -19,10 +20,57 @@ const Appointments = () => {
   const columns = [
     {
       title: "Name",
-      dataIndex: "name",
-      key: "name",
+      dataIndex: "serviceName",
+      key: "serviceName",
+    },
+    {
+      title: "Appointment Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Appointment Time",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "Action",
+      dataIndex: "status",
+      key: "status",
+      render: (text, record) => {
+        return (
+          <div>
+            {record?.status === "pending" ? <button onClick={() => handleCancel(record)} className="bg-red-500 text-white px-4 py-2 rounded-md">
+              Cancel
+            </button> : <button disabled className="bg-green-500 text-white px-4 py-2 rounded-md">
+              Delivered
+            </button>}
+          </div>
+        );
+      }
     }
   ]
+
+  const handleCancel = async (record) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  }
 
   const handleTableChange = async (data) => {
     
@@ -32,6 +80,8 @@ const Appointments = () => {
     <>
       <Title text={"My Appointments"} />
       <Table
+      dataSource={data?.data}
+       columns={columns}
         className="w-full overflow-auto"
         pagination={{
           pageSize: pagination?.pageSize,
