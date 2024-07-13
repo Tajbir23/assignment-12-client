b09fa701b78e921a3877d117aa5a5d6b9466f9e7
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-13 18:12:24 +0600

    contact redesign

--- src/pages/Home/ContactUs.jsx
+++ src/pages/Home/ContactUs.jsx
@@ -2,10 +2,11 @@
 
 const ContactUs = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
-      <div className="bg-white rounded-lg shadow-lg p-8 max-w-md">
+    <div className=" w-full flex justify-center items-center">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-full md:mx-20 sm:mx-10 mx-5">
         <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
         <form className="space-y-4">
+          <div className="md:flex gap-10">
           <div>
             <label htmlFor="name" className="block font-semibold">Name</label>
             <input type="text" id="name" name="name" placeholder="Your Name" className="w-full border-gray-300 border rounded-md px-4 py-2 focus:outline-none focus:border-blue-500" />
@@ -13,6 +14,7 @@ const ContactUs = () => {
           <div>
             <label htmlFor="email" className="block font-semibold">Email</label>
             <input type="email" id="email" name="email" placeholder="Your Email" className="w-full border-gray-300 border rounded-md px-4 py-2 focus:outline-none focus:border-blue-500" />
+          </div>
           </div>
           <div>
             <label htmlFor="message" className="block font-semibold">Message</label>
